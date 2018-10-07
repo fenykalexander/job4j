412169ef8aa4bc0a1090cbd3a73861c54844b7ce
fenukalexander <aleksandar.fenyuk@yandex.ru>
2018-10-07 11:31:58 +0300

    Maven import

--- chapter_001/src/main/java/ru.job4j.calculate/Calculate.java
+++ chapter_001/src/main/java/ru.job4j.calculate/Calculate.java
@@ -1,27 +1,31 @@
-/**
- * Task
- */
 
 package ru.job4j.calculate;
 
 /**
  * Calculate.
- * @autor Fenyk Alexander(aleksandar.fenyuk@yandex.ru)
+ *  @author Fenyk Alexander(aleksandar.fenyuk@yandex.ru)
  * @since 21/092018
  */
 public class Calculate {
 	/**
-	 * Main
-	 * @param args-args
+	 * Main.
+	 * @param args-args.
 	 */
 	public static void main(String[] args)
+
 	{
 		System.out.println("Hello World");
 	}
-//проверь все еще раз
-	//чекстаил установлен не верно
-	// исправь плугин который я закоментировал там ошибка
-	// все ошибки по невнимательности к мелочам
-	//в класе калкулейт нет метода echo как его вызвать в тестах??
-	//я пока бегу если что пишите в задаче
-}
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+
+	public void String echo (String name){
+
+		return "Echo, echo, echo : " + name;
+	}
+	}
+
+
--- chapter_001/src/test/java/ru.job4j.calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru.job4j.calculate/CalculateTest.java
@@ -1,25 +1,30 @@
 package ru.job4j.calculate;
 
-
-
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+import org.junit.Test;
 
 /**
+ * Test
  * CalculateTest.
  * @autor Fenyk Alexander(aleksandar.fenyuk@yandex.ru)
  * @since 21/092018
- * Test
+ *
  */
 public class CalculateTest {
 	/**
-	 * Test echo
+	 *Test echo
 	 */
-    public  void whenTakeNameThenTreeEchoPlusName() {
+    //public  void whenTakeNameThenTreeEchoPlusName() {
 
-		String input = "Fenyk Alexandar";
-		String expect = "Echo, echo, echo : Fenyk Alexandar";
-		Calculate calc = new Calculate();
+		//String input = "Fenyk Alexandar";
+		//String expect = "Echo, echo, echo : Fenyk Alexandar";
+		//Calculate calculate = new Calculate();
+		//String result = calculate.eсho(name);
+		//assertThat(result,is(expect));
+
 
 
 	}
 
-}
+//}
